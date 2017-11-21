e09acc78b764180d50c825bed7c149b63ad7bb06
michaelkarl18 <michaelkarl18@newmanschool.org>
2017-11-21 11:09:50 -0600

    I added a start screen

--- newsketch.js
+++ newsketch.js
@@ -1,76 +1,84 @@
 var words = ["cat","dog","bird","elephant"];
-var randomWord = words[Math.floor[Math.random()*words.length]]
+var randomNumber = Math.floor[Math.random()*words.length];
+var randomWord = words[randomNumber]
+var wordSplit = randomWord.split("");
+var guesses[]
 var guessesRight = [];
 var guessesWrong = [];
-var answerArray = [];
-var count = 0;
+var underScore = [];
+var numGuessesWrong = 0;
+var userGuess = 0;
 
 function setup(){
   createCanvas(1000,1000)
 }
 
-function drawBlanks(){
-  for (var i = 0; i < randomWord.length; i++){
-    answerArray[i]= "_"
+function draw(){
+    generateUnderScores();
+    guess();
+    drawGuesses();
 }
 
-function draw(){
-  background(255);
-  guess();
-  drawGuesses();
-  drawBlanks();
-  drawHangman();
+function generateUnderScores(){
+  for (var i =0; i<randomWord.length; i++){
+  underScore.push('_')
 }
+  var output = '';
+  for(var x=0; x<underScore.length; x++){
+      output+= underScore[x] + " ";
+  }
+
+  }
 
 function guess(){
-  if (count == 0){
-    text("Enter a letter.",450,30)
-  }
-  else if (count == 1){
-    text()
+  while (numGuessesWrong == 0){
+    userGuess = prompt("Enter a letter.")
+    while (guesses.indexOf(userGuess.toLowerCase()) >= 0) {
+     alert("You have already guessed that letter. Try again");
+     userGuess = prompt("What letter would you like to guess?");
+ }
+
+ for (x = 0; x < wordSplit.length; x++) {
+
+     if (userGuess == wordSplit[x]) {
+         guesses.push(userGuess);
+         underscore.replace([x], userGuess);
+     else if (userGuess != wordSplit[x]) {
+         guessesWrong.push(userGuess);
+         numGuessesWrong+;
+     }
+     else if (userGuess != secretWord && numGuessesWrong === 7) {
+
+         alert("Game Over");
+     }
+ element.innerHTML = guessesWrong;
   }
 }
-
-function keyTyped(){
-  if (count == 0){
-    guessesRight.push(key);
-    putGuessesInArray(key);
-    console.log(key);
-    screen ++;
-  }
-}
-
 function drawGuesses(){
-  for(var i =0; i<guessesRight.length; i++){
-  var num = word.indexOf(guess[i]);
-    text(guessesRight[i],100,100)
-  }
-}
-
-function drawHangman(){
 rect(100,50,150,1);
 rect(100,50,1,350);
 rect(35,400,150,1);
 rect(250,50,1,50);
-if (count == 1){
-  ellipse(250,120,60);
 }
-if(count == 2){
-  rect(250,150,1,150);
-}
-if(count == 3){
-  line(250, 200, 175, 175);
-}
-if(count == 4){
-  line(250, 200, 325, 180);
-}
-if(count == 5){
-  line(250, 300, 200, 340);
-}
-if(count == 6){
-  line(250, 300, 300, 345);
-}
-if(count = 7){
-  text("You lose!!!!!!!!")
-}
+
+  if (numGuessesWrong = 1){
+    ellipse(250,120,0);
+  }
+  if(numGuessesWrong = 2){
+    rect(250,150,1,150);
+  }
+  if(numGuessesWrong = 3){
+    line(250, 200, 175, 175);
+  }
+  if(numGuessesWrong = 4){
+    line(250, 200, 325, 180);
+  }
+  if(numGuessesWrong = 5){
+    line(250, 300, 200, 340);
+  }
+  if(numGuessesWrong = 6){
+    line(250, 300, 300, 345);
+  }
+   }
+
 }
